3f01e52a4e2a196623429e809c5f871a775c75e6
Angela Roosmägi <angela.roosmagi@voco.ee>
2024-11-15 15:12:51 +0200

    Split first custom component into multible components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,11 @@
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
 
-    
-
-    console.log(props)
-    console.log(props.data)
-
     return (
         <div className='expense-item'> 
-            <div>{props.data.date.toString()}</div>
+            <ExpenseDate date={props.data.date}/> 
             <div className="expense-item__description">
                 <h2>{props.data.title}</h2>
                 <div className="expense-item__price">{props.data.price}</div>
