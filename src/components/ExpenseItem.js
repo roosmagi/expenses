58b4a6320e21fb5958321accb41204efc167b30d
Angela Roosmägi <angela.roosmagi@voco.ee>
2024-11-19 21:26:10 +0200

    Add Card component to ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
+import Card from './Card'
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
 
     return (
-        <div className='expense-item'> 
+        <Card className='expense-item'> 
             <ExpenseDate date={props.data.date}/> 
             <div className="expense-item__description">
                 <h2>{props.data.title}</h2>
                 <div className="expense-item__price">{props.data.price}</div>
             </div>
-        </div>
+        </Card>
     )
 } 
 
